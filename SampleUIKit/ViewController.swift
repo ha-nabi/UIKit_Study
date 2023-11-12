6520e3ddf750f1c15d8d10bdf860dedab1578904
ciu <112779139+kangciu@users.noreply.github.com>
2023-11-12 19:24:34 +0900

    Add: UiButton

--- SampleUIKit/ViewController.swift
+++ SampleUIKit/ViewController.swift
@@ -17,6 +17,14 @@ class ViewController: UIViewController {
         label.backgroundColor = .green
         return label
     }()
+    
+    lazy var ChangeButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false // 우리가 이제 오토레이아웃을 코드로 쓸거다 라고 코드로 적는 부분
+        button.setTitle("안녕하세요", for: .normal)
+        button.backgroundColor = .blue
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,5 +36,16 @@ class ViewController: UIViewController {
         myNameTextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         myNameTextLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
+        view.addSubview(ChangeButton)
+        ChangeButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        ChangeButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        ChangeButton.topAnchor.constraint(equalTo: myNameTextLabel.bottomAnchor, constant: 200).isActive = true
+        ChangeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        ChangeButton.addTarget(self, action: #selector(didTapChangeNameButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapChangeNameButton() {
+        myNameTextLabel.text = "안녕하세용가리"
     }
 }
